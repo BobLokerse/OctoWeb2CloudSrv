e5f74ec27f62e745b23dcd33ca602d7599bc4a2d
Bob Lokerse <bob.lokerse@gmail.com>
2016-06-16 14:12:31 +0200

    Initial web project

---
+++ OctoWeb2CloudSrv/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OctoWeb2CloudSrv.MvcApplication" Language="C#" %>
---
+++ OctoWeb2CloudSrv/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OctoWeb2CloudSrv.Startup))]
+namespace OctoWeb2CloudSrv
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
